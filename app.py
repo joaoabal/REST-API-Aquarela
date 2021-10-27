c9d02a95295c437897e7230cbc82a52c2f4e4e4c
joaoabal <joaoabal@users.noreply.github.com>
2021-10-27 10:30:42 -0300

    OpenApi (Swagger) adicionado!

--- app.py
+++ app.py
@@ -7,22 +7,23 @@ Created on Sun Oct 24 18:26:00 2021
 
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 from resources.colaborador import Colaboradores
 from resources.colaborador import Colaborador
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localhost:5001/dbteste"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 api = Api(app)
+cors = CORS(app)
 
 @app.before_first_request
 def cria_db():
     bd.create_all()
 
-
 api.add_resource(Colaboradores,"/colaborador")
 api.add_resource(Colaborador,"/colaborador/<string:matricula>")
-# api.add_resource(ColaboradorSenha,"/colaborador/<string:matricula>")
 
 if __name__ == "__main__":
     from sql_alchemy import bd
--- resources/colaborador.py
+++ resources/colaborador.py
@@ -1,8 +1,6 @@
 from flask_restful import Resource, reqparse
 from models.colaborador import ColaboradorModel
 
-
-
 class Colaboradores(Resource):
     def get(self):
         return {"colaboradores" : [colaborador.json() for colaborador in ColaboradorModel.query.all()] }        
@@ -64,53 +62,3 @@ class Colaborador(Resource):
             return {"message" : "Colaborador deletado."}
         return {"message" : "Colaborador não encontrado."} , 404
 
-
-# class ColaboradorSenha(Resource):
-
-#     argumentos = reqparse.RequestParser()
-#     argumentos.add_argument("senha", type=str, required=False)
-
-                
-#     def get(self, matricula):
-#         colaborador = ColaboradorModel.encontrar_colaborador(matricula)
-#         if colaborador:
-#                 return colaborador.json()
-#         return {"message" : "Colaborador nao encontrado"}, 404
-
-#     def post(self, matricula):
-#         if ColaboradorModel.encontrar_colaborador(matricula):
-#             return {'message': 'Matricula "{}" ja existe!'.format(matricula)} , 400 # bad request!
-
-#         dados = Colaborador.argumentos.parse_args()
-#         colaborador = ColaboradorModel(matricula, **dados)
-#         try:
-#             colaborador.save_colaborador()
-#         except:
-#             return {"message" : "Erro interno tentando salvar."} , 500 # internal server error
-#         return colaborador.json()
-
-#     def put(self, matricula):
-
-#         dados = Colaborador.argumentos.parse_args()
-
-#         colaborador_encontrado = ColaboradorModel.encontrar_colaborador(matricula)
-#         if colaborador_encontrado:
-#             colaborador_encontrado.update_colaborador(**dados)
-#             try:
-#                 colaborador_encontrado.save_colaborador()
-#             except:
-#                 return {"message" : "Erro interno tentando salvar."} , 500 # internal server error
-#             return colaborador_encontrado.json(), 200
-#         colaborador = ColaboradorModel(matricula, **dados)
-#         colaborador.save_colaborador()
-#         return colaborador.json(), 201 #criado!
-
-#     def delete(self, matricula):
-#         colaborador = ColaboradorModel.encontrar_colaborador(matricula)
-#         if colaborador:
-#             try:
-#                 colaborador.delete_colaborador()
-#             except:
-#                 return {"message" : "Erro interno tentando deletar."} , 500 # internal server error
-#             return {"message" : "Colaborador deletado."}
-#         return {"message" : "Colaborador não encontrado."} , 404
